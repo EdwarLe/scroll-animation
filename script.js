0c1388ef59c0113d7e1cd2ab4ec99beb9156c224
Edwar León <edwar.leon.m@gmail.com>
2023-06-02 16:51:47 -0500

    Se completa el ejercicio, modificando el Js creando la función que permite que salgan los box en el orden adecuado

--- script.js
+++ script.js
@@ -13,14 +13,48 @@ window.onscroll = () => {desplazamiento_lateral()};
 
 function desplazamiento_lateral () {
     imparBox.forEach(e => {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            imparBox[2].classList.add('content__show');
+        if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+            imparBox[2].classList.add('content__show__rigth');
         } else {
-            imparBox[2].classList.remove('content__show');
+            imparBox[2].classList.remove('content__show__rigth');
+        } if (document.body.scrollTop >= 250 || document.documentElement.scrollTop >= 250) {
+            imparBox[3].classList.add('content__show__left');
+        } else {
+            imparBox[3].classList.remove('content__show__left');
+        } if (document.body.scrollTop >= 450 || document.documentElement.scrollTop >= 450) {
+            imparBox[4].classList.add('content__show__rigth');
+        } else {
+            imparBox[4].classList.remove('content__show__rigth');
+        } if (document.body.scrollTop >= 650 || document.documentElement.scrollTop >= 650) {
+            imparBox[5].classList.add('content__show__left');
+        } else {
+            imparBox[5].classList.remove('content__show__left');
+        } if (document.body.scrollTop >= 850 || document.documentElement.scrollTop >= 850) {
+            imparBox[6].classList.add('content__show__rigth');
+        } else {
+            imparBox[6].classList.remove('content__show__rigth');
+        } if (document.body.scrollTop >= 1050 || document.documentElement.scrollTop >= 1050) {
+            imparBox[7].classList.add('content__show__left');
+        } else {
+            imparBox[7].classList.remove('content__show__left');
+        } if (document.body.scrollTop >= 1250 || document.documentElement.scrollTop >= 1250) {
+            imparBox[8].classList.add('content__show__rigth');
+        } else {
+            imparBox[8].classList.remove('content__show__rigth');
+        } if (document.body.scrollTop >= 1450 || document.documentElement.scrollTop >= 1450) {
+            imparBox[9].classList.add('content__show__left');
+        } else {
+            imparBox[9].classList.remove('content__show__left');
+        } if (document.body.scrollTop >= 1650 || document.documentElement.scrollTop >= 1650) {
+            imparBox[10].classList.add('content__show__rigth');
+        } else {
+            imparBox[10].classList.remove('content__show__rigth');
+        } if (document.body.scrollTop >= 1750 || document.documentElement.scrollTop >= 1750) {
+            imparBox[11].classList.add('content__show__left');
+        } else {
+            imparBox[11].classList.remove('content__show__left');
         }
         
     });
 }
-
-//verificación para la subida al github por error que sale desde el GitBash
 
